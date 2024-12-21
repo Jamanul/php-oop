3143279e426bed9d8457b1a282ec1b0b7fdadb52
jamanul <jk.skb.25@gmail.com>
2024-12-21 12:33:03 +0600

    contruct class properties

--- Transaction.php
+++ Transaction.php
@@ -3,4 +3,48 @@
 class Transaction {
    public float $amount;
    public string $descirption;
+
+    public function __construct(float $amount , string $descirption) {
+    $this->amount = $amount;
+    $this->descirption =$descirption;
+   }
+   public function addTax (float $tax): void{
+    $this->amount += $this->amount *$tax /100; 
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Define a class
+class Car {
+    // Properties
+    public $brand;
+    public $color;
+    
+    // Constructor
+    public function __construct($brand, $color) {
+        $this->brand = $brand;  // Use $this to refer to the object's property
+        $this->color = $color;  // Initialize property with the provided value
+        echo "A new car object is created! Brand: $brand, Color: $color<br>";
+    }
+    
+    // Method to display car details
+    public function displayDetails() {
+        echo "This car is a " . $this->color . " " . $this->brand . ".<br>";
+    }
+    
+    // Destructor
+    public function __destruct() {
+        echo "The car object for {$this->brand} is destroyed.<br>";
+    }
+}
+
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php 
 require_once("./Transaction.php");
-$transaction = new Transaction();
-$transaction -> $amount =15; 
-var_dump($transaction -> $amount);
+$transaction = new Transaction(100,"transaction 1");
+$transaction ->addTax(8);
+var_dump($transaction->amount );
+
+// Create an object (instance of the class)
+// $car1 = new Car("Toyota", "Red");
+// $car1->displayDetails();  
+
+// // Create another object
+// $car2 = new Car("Honda", "Blue");
+// $car2->displayDetails();  
+
+
